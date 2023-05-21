51266d68c92c817ef798e5a40c710d541079562e
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2023-05-21 11:19:34 +0700

    add entity (account, category, product)

---
+++ src/java/entity/Product.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author minhhieu
+ */
+public class Product {
+    private int pId;
+    private String pName;
+    private String pImg;
+    private float pPrice;
+    private String pDetail;
+
+    public Product() {
+    }
+
+    public Product(int pId, String pName, String pImg, float pPrice, String pDetail) {
+        this.pId = pId;
+        this.pName = pName;
+        this.pImg = pImg;
+        this.pPrice = pPrice;
+        this.pDetail = pDetail;
+    }
+
+    public int getpId() {
+        return pId;
+    }
+
+    public void setpId(int pId) {
+        this.pId = pId;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getpImg() {
+        return pImg;
+    }
+
+    public void setpImg(String pImg) {
+        this.pImg = pImg;
+    }
+
+    public float getpPrice() {
+        return pPrice;
+    }
+
+    public void setpPrice(float pPrice) {
+        this.pPrice = pPrice;
+    }
+
+    public String getpDetail() {
+        return pDetail;
+    }
+
+    public void setpDetail(String pDetail) {
+        this.pDetail = pDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "pId=" + pId + ", pName=" + pName + ", pImg=" + pImg + ", pPrice=" + pPrice + ", pDetail=" + pDetail + '}';
+    }
+   
+    
+}
